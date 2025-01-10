ffe29650c28dff2cbd55aaa86ad4503430aabacd
Adriano Sanderson <adrianoramos06@gmail.com>
2025-01-09 21:10:51 -0300

    Continuação do desafio.

--- 02-React/21-Desafio-Router-1/src/Produto.jsx
+++ 02-React/21-Desafio-Router-1/src/Produto.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 function Produto() {
-    const params =useParams()
-    console.log(params)
+  const [produto, setProduto] = useState(null)
+  const [carregando, setCarregando] = useState(false)
+  const [erro, setErro] = useState(null)
+  const { id } = useParams()
+
+  useEffect(() => {
+    async function fetchProduto() {
+      try{
+        setCarregando(true)
+        const response = await fetch(`https://ranekapi.origamid.dev/json/api/produto/${id}`)
+        const json = await response.json()
+        setProduto(json)
+      } catch (error) {
+        setErro('Erro ao carregar o produto')
+      } finally {
+        setCarregando(false)
+      }
+    }
+    fetchProduto()
+  },[id])
+
+  
+  if (carregando) return <p>Carregando...</p>
+  if (erro) return <p>{erro}</p>
+  if (produto === null) return null
   return (
-    <div>Produto</div>
+    <div>
+      <h1>{produto.nome}</h1>
+      <img src={produto.fotos[0].src} width="400" alt="" /><br />
+      <span>Preço: R$ {produto.preco}</span>
+    </div>
   )
 }
 
